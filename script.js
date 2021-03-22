9f3a338a156e832fd5aeb3803521b0db105fcdd1
Riley Hunter <postagestamp@windowslive.com>
2021-03-22 22:55:31 +1300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ var phrases = [
     'You\'re perfect just the way you are',
     'Do not march on Moscow',
     'I\'d let you test my software until it was hardware ;)',
-    'You\'re beautiful on the inside. Trust me, I checked'
+    'You\'re beautiful on the inside. Trust me, I checked',
+    'We should grow potatoes together after the apocalypse happens',
+    'We should intertwine our old, wrinkly fingers in 2069',
+    '*saxophone intro from Careless Whisper*',
+    'You make me UwU',
+    'If I had an imaginary friend it\'d probably still be you',
+    'BEGIN TRANSACTION SELECT \'<3\' AS [Message To You] ROLLBACK',
+    'Nice butt',
+    'If we were two zombies I would sit in a boat with you',
+    'MmmMMMMMmMMMMMMmMmnmMmMmmmmmMMMMM'
 ]
 
 var contentDiv = document.getElementById('content');
